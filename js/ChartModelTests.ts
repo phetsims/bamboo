22719e4f8e45730fb665c2e375ad267d276551df
zepumph <michael.kauzmann@colorado.edu>
2022-08-11 13:57:14 -0600

    add dot-notation lint rule and fix issues, https://github.com/phetsims/chipper/issues/1295

--- js/ChartModelTests.ts
+++ js/ChartModelTests.ts
@@ -6,9 +6,12 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-// @ts-ignore
+import BarPlot from './BarPlot.js';
+import ChartTransform from './ChartTransform.js';
+
 QUnit.module( 'ChartModel' );
 
 QUnit.test( 'basic tests', assert => {
   assert.ok( true, 'first test' );
+  assert.ok( new BarPlot( new ChartTransform(), [] ), 'another simple test' );
 } );

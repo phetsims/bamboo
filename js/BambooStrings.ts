f15fbbe15778607e59925da1a0284aae9b332b6e
Sam Reid <reids@colorado.edu>
2022-09-26 12:16:49 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/BambooStrings.ts
+++ js/BambooStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import bamboo from './bamboo.js';
 
 type StringsType = {
   'bamboo': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 

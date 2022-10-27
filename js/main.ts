f1dc7766a3f685722c6f5ab1423d64922bee0861
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:09 -0600

    Updating phet-lib with main common code references.

---
+++ js/main.ts
@@ -0,0 +1,24 @@
+// Copyright 2022, University of Colorado Boulder
+
+import bamboo from './bamboo.js';
+import './AxisArrowNode.js';
+import './AxisLine.js';
+import './BambooStrings.js';
+import './BarPlot.js';
+import './CanvasGridLineSet.js';
+import './CanvasLinePlot.js';
+import './CanvasPainter.js';
+import './ChartCanvasNode.js';
+import './ChartRectangle.js';
+import './ChartTransform.js';
+import './ClippingType.js';
+import './GridLineSet.js';
+import './LinePlot.js';
+import './LinearEquationPlot.js';
+import './ScatterPlot.js';
+import './SpanNode.js';
+import './TickLabelSet.js';
+import './TickMarkSet.js';
+import './UpDownArrowPlot.js';
+
+export default bamboo;

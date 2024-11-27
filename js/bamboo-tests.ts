cfc0e0c6728a454d226e7a903b32681684d82a83
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-27 15:23:38 -0700

    move browser files in chipper into a sub directory, https://github.com/phetsims/chipper/issues/1534

--- js/bamboo-tests.ts
+++ js/bamboo-tests.ts
@@ -7,7 +7,7 @@
  */
 
 import './ChartModelTests.js';
-import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+import qunitStart from '../../chipper/js/browser/sim-tests/qunitStart.js';
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
 qunitStart();
